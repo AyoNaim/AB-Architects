af905d13e4b994eedaa611e3f2307507212f9649
AyoNaim Mide <ayonaim101@gmail.com>
2024-07-21 00:10:57 +0000

    made changes

--- abarchitects/components/Hero.tsx
+++ abarchitects/components/Hero.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='w-full h-96  border-b'>
+        <div>
+            <div className='flex justify-center items-center flex-col'>
+                <p className='font-bold text-[35px]'>ARCHITECTURAL SOLUTIONS</p>
+                <Button variant={'secondary'} className='bg-gray-900 mt-4 text-white text-sm hover:bg-gray-600'>Contact us</Button>
+                {/* <Image src={'/build.png'} alt='build' width={300} height={300}/> */}
+            </div>
+        </div>
+    </section>
   )
 }
 
